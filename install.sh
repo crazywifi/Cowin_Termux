05cdc99f74ad551f220419e0efd268112db85e67
Rishabh Sharma <rishabh2241991@gmail.com>
2021-06-06 22:36:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
+echo "deb https://termux.org/packages/ stable main" > /data/data/com.termux/files/usr/etc/apt/sources.list
+cp /data/data/com.termux/files/usr/etc/apt/sources.list.d/game.list game.list.backup
+rm /data/data/com.termux/files/usr/etc/apt/sources.list.d/game.list
+cp /data/data/com.termux/files/usr/etc/apt/sources.list.d/science.list science.list.backup
+rm /data/data/com.termux/files/usr/etc/apt/sources.list.d/science.list
+pkg update
 pkg install python termux-api mpv git
 pip install requests
 pip3 install requests
 git clone https://github.com/crazywifi/Cowin_Termux.git
+cd Cowin_Termux
